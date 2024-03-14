17f1753afffdd8642cf00b5e69db8b028b122b90
Dusk <59524589+naver8324@users.noreply.github.com>
2024-03-15 00:29:31 +0900

    [level 1] Title: 추억 점수, Time: 0.04 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.java
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.java
@@ -3,17 +3,19 @@ import java.util.*;
 class Solution {
     public int[] solution(String[] name, int[] yearning, String[][] photo) {
         int[] answer = new int[photo.length];
-        HashMap<String, Integer> map = new HashMap<>();
+        HashMap<String, Integer> nameYearningMap = new HashMap<>();
 
+        // 이름과 갈망도를 매핑
         for (int i = 0; i < name.length; i++) {
-            map.put(name[i], yearning[i]);
+            nameYearningMap.put(name[i], yearning[i]);
         }
 
-
+        // 각 사진별로 그리움 점수 계산
         for (int i = 0; i < photo.length; i++) {
             for (int j = 0; j < photo[i].length; j++) {
-                if (map.containsKey(photo[i][j])) {
-                    answer[i] += map.get(photo[i][j]);
+                if (nameYearningMap.containsKey(photo[i][j])) {
+                    // 그리움 점수 더하기
+                    answer[i] += nameYearningMap.get(photo[i][j]);
                 }
             }
         }
