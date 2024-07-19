edca1a06c11e62681b9f89719a7c77dc703f2ba7
Dusk <59524589+naver8324@users.noreply.github.com>
2024-07-20 07:49:39 +0900

    [Bronze I] Title: 평균, Time: 264 ms, Memory: 18428 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,22 +1,19 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int examCount = sc.nextInt();
         int[] examScores = new int[examCount];
+        double sum = 0;
 
         for (int i = 0; i < examCount; i++) {
             examScores[i] = sc.nextInt();
+            sum += examScores[i];
         }
+
         int max = Arrays.stream(examScores).max().orElseThrow();
-        double average = 0;
 
-        for (int n : examScores) {
-            average += (double) n /max*100;
-        }
-
-        System.out.println(average/examCount);
+        System.out.println(sum * 100 / max / examCount);
     }
 }
