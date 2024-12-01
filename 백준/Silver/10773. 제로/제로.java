5244d120bf6178ec7200b7d1635b10745acff391
Dusk <59524589+naver8324@users.noreply.github.com>
2024-12-01 15:33:24 +0900

    [Silver IV] Title: 제로, Time: 216 ms, Memory: 23920 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int K = Integer.parseInt(br.readLine());
+        Stack<Integer> ledger = new Stack<>();
+
+        for (int i = 0; i < K; i++) {
+            int money = Integer.parseInt(br.readLine());
+            if (money == 0) {
+                ledger.pop();
+            } else {
+                ledger.push(money);
+            }
+        }
+
+        int sum = 0;
+        for (int i : ledger) {
+            sum += i;
+        }
+
+        System.out.println(sum);
+    }
+}
