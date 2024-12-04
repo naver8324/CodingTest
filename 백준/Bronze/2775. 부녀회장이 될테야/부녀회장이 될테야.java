bf293b68abb6e78acabbc4a932218eed823109df
Dusk <59524589+naver8324@users.noreply.github.com>
2024-12-05 03:34:08 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 116 ms, Memory: 14380 KB -BaekjoonHub

--- 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.java
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.java
@@ -7,25 +7,26 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int T = Integer.parseInt(br.readLine());
 
+        int[][] apartment = new int[15][15];
+
+        for (int i = 1; i < 15; i++) {
+            apartment[0][i] = i;
+        }
+        for (int k = 1; k < 15; k++) {
+            apartment[k][1] = 1;
+            for (int n = 2; n < 15; n++) {
+                apartment[k][n] = apartment[k - 1][n] + apartment[k][n - 1];
+            }
+        }
+
         for (int i = 0; i < T; i++) {
             int k = Integer.parseInt(br.readLine());
             int n = Integer.parseInt(br.readLine());
-            bw.write(calculate(k, n)+"\n");
+            bw.write(apartment[k][n]+"\n");
         }
 
         bw.flush();
         br.close();
         bw.close();
-    }
-
-    private static int calculate(int k, int n) {
-        if (k == 0) {
-            return n;
-        }
-        if (n == 1) {
-            return 1;
-        }
-
-        return calculate(k - 1, n) + calculate(k, n - 1);
     }
 }
