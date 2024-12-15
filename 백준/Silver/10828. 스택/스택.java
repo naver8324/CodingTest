224a3f876e60629f7d5b44ccb409c7dca071e269
Dusk <59524589+naver8324@users.noreply.github.com>
2024-12-16 01:46:39 +0900

    [Silver IV] Title: 스택, Time: 164 ms, Memory: 20664 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < N; i++) {
+            String[] command = br.readLine().split(" ");
+            
+            switch (command[0]) {
+                case "push":
+                    stack.push(Integer.parseInt(command[1]));
+                    break;
+                case "pop":
+                    sb.append(stack.isEmpty() ? -1 : stack.pop()).append("\n");
+                    break;
+                case "size":
+                    sb.append(stack.size()).append("\n");
+                    break;
+                case "empty":
+                    sb.append(stack.isEmpty() ? 1 : 0).append("\n");
+                    break;
+                case "top":
+                    sb.append(stack.isEmpty() ? -1 : stack.peek()).append("\n");
+                    break;
+            }
+        }
+
+        System.out.print(sb);
+    }
+}
