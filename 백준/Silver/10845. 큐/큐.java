81508b0f4c51d837d5db233913dfc3ba858d65f9
Dusk <59524589+naver8324@users.noreply.github.com>
2024-12-17 05:25:10 +0900

    [Silver IV] Title: 큐, Time: 180 ms, Memory: 20724 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        
+        int N = Integer.parseInt(br.readLine());
+        Deque<Integer> queue = new LinkedList<>();
+        
+        for (int i = 0; i < N; i++) {
+            String[] command = br.readLine().split(" ");
+            
+            switch (command[0]) {
+                case "push":
+                    queue.add(Integer.parseInt(command[1]));
+                    break;
+                case "pop":
+                    sb.append(queue.isEmpty() ? -1 : queue.poll()).append("\n");
+                    break;
+                case "size":
+                    sb.append(queue.size()).append("\n");
+                    break;
+                case "empty":
+                    sb.append(queue.isEmpty() ? 1 : 0).append("\n");
+                    break;
+                case "front":
+                    sb.append(queue.isEmpty() ? -1 : queue.peek()).append("\n");
+                    break;
+                case "back":
+                    sb.append(queue.isEmpty() ? -1 : queue.peekLast()).append("\n");
+                    break;
+            }
+        }
+        
+        System.out.print(sb);
+    }
+}
