a8fbc9379d6cc0e8935f579cabd36a7fd6e3b1b4
Dusk <59524589+naver8324@users.noreply.github.com>
2025-01-13 19:59:01 +0900

    [Bronze V] Title: 개, Time: 100 ms, Memory: 13904 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("|\\_/|").append("\n");
+        sb.append("|q p|   /}").append("\n");
+        sb.append("( 0 )\"\"\"\\").append("\n");
+        sb.append("|\"^\"`    |").append("\n");
+        sb.append("||_/=\\\\__|");
+
+        System.out.print(sb.toString());
+    }
+}
