2d761318dfe69f3045823a5ac67e1b86d02e0cc8
Dusk <59524589+naver8324@users.noreply.github.com>
2025-01-15 21:19:09 +0900

    [Bronze V] Title: 고양이, Time: 108 ms, Memory: 13960 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        sb.append("\\    /\\").append("\n")
+                .append(" )  ( ')").append("\n")
+                .append("(  /  )").append("\n")
+                .append(" \\(__)|");
+
+        System.out.print(sb.toString());
+    }
+}
