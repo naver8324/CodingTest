ee2d72d969bf8b2373c7b957a6a5c97704a3e1dc
Dusk <59524589+naver8324@users.noreply.github.com>
2025-01-28 16:29:39 +0900

    Create 파티가 끝나고 난 뒤.java

---
+++ 백준/Bronze/2845. 파티가 끝나고 난 뒤/파티가 끝나고 난 뒤.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      	StringTokenizer st = new StringTokenizer(br.readLine());
+      	StringBuilder sb = new StringBuilder();
+      
+      	int L = Integer.parseInt(st.nextToken());
+      	int P = Integer.parseInt(st.nextToken());
+      
+      	int peopleCount = L * P;
+      	st = new StringTokenizer(br.readLine());
+      
+      	for(int i=0; i<5; i++){
+      		int participantCount = Integer.parseInt(st.nextToken());
+      		sb.append(participantCount - peopleCount).append(" ");
+      	}
+      
+      	System.out.println(sb.toString());
+      	br.close();
+    }
+}
