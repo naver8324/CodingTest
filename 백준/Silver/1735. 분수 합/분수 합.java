8af20db0e289b07d4108e3a5fb35986693fba545
Dusk <59524589+naver8324@users.noreply.github.com>
2025-03-01 01:35:54 +0900

    [Silver III] Title: 분수 합, Time: 124 ms, Memory: 15968 KB -BaekjoonHub

---
+++ 백준/Silver/1735. 분수 합/분수 합.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        int C = Integer.parseInt(st.nextToken());
+        int D = Integer.parseInt(st.nextToken());
+
+        A *= D;
+        C *= B;
+        B *= D;
+        A += C;
+
+        int gcd = gcd(A, B);
+
+        System.out.println(A / gcd + " " + B / gcd);
+        br.close();
+    }
+
+    private static int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = a % b;
+            a = b;
+            b = temp;
+        }
+
+        return a;
+    }
+}
