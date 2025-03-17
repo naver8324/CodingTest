a33d766afe95d0649d7cafb8618c141ae49892d1
Dusk <59524589+naver8324@users.noreply.github.com>
2025-03-17 17:35:16 +0900

    [Silver III] Title: 파도반 수열, Time: 100 ms, Memory: 14080 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.java
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.java
@@ -11,9 +11,11 @@ public class Main {
         P[1] = 1;
         P[2] = 1;
         P[3] = 1;
+        P[4] = 2;
+        P[5] = 2;
 
-        for (int i = 4; i < 101; i++) {
-            P[i] = P[i - 2] + P[i - 3];
+        for (int i = 6; i < 101; i++) {
+            P[i] = P[i - 1] + P[i - 5];
         }
 
         StringBuilder sb = new StringBuilder();
