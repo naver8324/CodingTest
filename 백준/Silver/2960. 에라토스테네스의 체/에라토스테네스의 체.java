384e267bcc46671978a27b76237bbe4c92c5bbd3
Dusk <59524589+naver8324@users.noreply.github.com>
2025-03-21 07:03:19 +0900

    [Silver IV] Title: 에라토스테네스의 체, Time: 104 ms, Memory: 13996 KB -BaekjoonHub

--- 백준/Silver/2960. 에라토스테네스의 체/에라토스테네스의 체.java
+++ 백준/Silver/2960. 에라토스테네스의 체/에라토스테네스의 체.java
@@ -1,30 +1,33 @@
+import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int K = sc.nextInt();
-        sc.close();
-        
-        boolean[] isPrime = new boolean[N + 1];
-        Arrays.fill(isPrime, true);
-        isPrime[0] = isPrime[1] = false;
-        
-        int count = 0;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N+1];
+        for (int i = 1; i <= N; i++) {
+            arr[i] = i;
+        }
+
         for (int i = 2; i <= N; i++) {
-            if (isPrime[i]) {
-                for (int j = i; j <= N; j += i) {
-                    if (isPrime[j]) {
-                        isPrime[j] = false;
-                        count++;
-                        if (count == K) {
-                            System.out.println(j);
-                            return;
-                        }
-                    }
+            for (int x = i; x <= N; x += i) {
+                if (arr[x] == 0) {
+                    continue;
+                }
+
+                arr[x] = 0;
+                K--;
+
+                if (K == 0) {
+                    System.out.println(x);
+                    return;
                 }
             }
         }
+
     }
 }
