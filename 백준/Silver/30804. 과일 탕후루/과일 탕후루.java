63db92376adac1bfadcccb8618c8b4e81a0f5b30
Dusk <59524589+naver8324@users.noreply.github.com>
2025-03-26 12:18:13 +0900

    [Silver II] Title: 과일 탕후루, Time: 440 ms, Memory: 36568 KB -BaekjoonHub

--- 백준/Silver/30804. 과일 탕후루/과일 탕후루.java
+++ 백준/Silver/30804. 과일 탕후루/과일 탕후루.java
@@ -2,13 +2,14 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
         int[] fruits = new int[N];
 
+        StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            fruits[i] = sc.nextInt();
+            fruits[i] = Integer.parseInt(st.nextToken());
         }
 
         System.out.println(getMaxTanghuluLength(fruits, N));
