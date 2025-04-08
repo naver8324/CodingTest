f376019cf6335edfdb5cb7bceccfb0dec87a25ea
Dusk <59524589+naver8324@users.noreply.github.com>
2025-04-08 23:53:07 +0900

    [Silver II] Title: 과일 탕후루, Time: 416 ms, Memory: 37580 KB -BaekjoonHub

--- 백준/Silver/30804. 과일 탕후루/과일 탕후루.java
+++ 백준/Silver/30804. 과일 탕후루/과일 탕후루.java
@@ -1,38 +1,38 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int[] fruits = new int[N];
+public class Main{
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[] fruits = new int[N];
+		Map<Integer, Integer> fruitsCount = new HashMap<>();
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            fruits[i] = Integer.parseInt(st.nextToken());
-        }
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
-        System.out.println(getMaxTanghuluLength(fruits, N));
-    }
+		for(int i=0; i<N; i++){
+			fruits[i] = Integer.parseInt(st.nextToken());
+		}
 
-    public static int getMaxTanghuluLength(int[] fruits, int N) {
-        Map<Integer, Integer> countMap = new HashMap<>();
-        int left = 0, maxLen = 0;
 
-        for (int right = 0; right < N; right++) {
-            countMap.put(fruits[right], countMap.getOrDefault(fruits[right], 0) + 1);
+        int maxLength = 0;
+        int left = 0;
 
-            while (countMap.size() > 2) {
-                countMap.put(fruits[left], countMap.get(fruits[left]) - 1);
-                if (countMap.get(fruits[left]) == 0) {
-                    countMap.remove(fruits[left]);
+        for(int right=0; right<N; right++){
+            fruitsCount.put(fruits[right], fruitsCount.getOrDefault(fruits[right], 0) + 1);
+
+            while(fruitsCount.size() > 2){
+                fruitsCount.put(fruits[left], fruitsCount.get(fruits[left])-1);
+                
+                if(fruitsCount.get(fruits[left]) == 0){
+                    fruitsCount.remove(fruits[left]);    
                 }
                 left++;
             }
 
-            maxLen = Math.max(maxLen, right - left + 1);
+            maxLength = Math.max(maxLength, right - left + 1);
         }
-
-        return maxLen;
-    }
+        
+		System.out.print(maxLength);
+	}
 }
