fe324125cdb6bc9aed55755ece7bfb9294e9680b
Dusk <59524589+naver8324@users.noreply.github.com>
2025-04-09 18:14:47 +0900

    [Silver V] Title: 돌 게임, Time: 104 ms, Memory: 14372 KB -BaekjoonHub

--- 백준/Silver/9655. 돌 게임/돌 게임.java
+++ 백준/Silver/9655. 돌 게임/돌 게임.java
@@ -1,11 +1,12 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
 
-        if (n % 2 == 1) {
+        if (N % 2 != 0) {
             System.out.println("SK");
         } else {
             System.out.println("CY");
