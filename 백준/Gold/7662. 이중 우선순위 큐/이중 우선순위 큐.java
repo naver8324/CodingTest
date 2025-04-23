08871700b4c86df43b32713b983aad67c7724221
Dusk <59524589+naver8324@users.noreply.github.com>
2025-04-24 03:23:34 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 2620 ms, Memory: 430320 KB -BaekjoonHub

---
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        while (T-- > 0) {
+            int k = Integer.parseInt(br.readLine());
+            TreeMap<Integer, Integer> map = new TreeMap<>();
+
+            for (int i = 0; i < k; i++) {
+                String[] input = br.readLine().split(" ");
+                char command = input[0].charAt(0);
+                int n = Integer.parseInt(input[1]);
+
+                if (command == 'I') {
+                    map.put(n, map.getOrDefault(n, 0) + 1);
+                } else if (command == 'D') {
+                    if (map.isEmpty()) continue;
+
+                    int key = (n == 1) ? map.lastKey() : map.firstKey();
+                    if (map.get(key) == 1) {
+                        map.remove(key);
+                    } else {
+                        map.put(key, map.get(key) - 1);
+                    }
+                }
+            }
+
+            if (map.isEmpty()) {
+                System.out.println("EMPTY");
+            } else {
+                System.out.println(map.lastKey() + " " + map.firstKey());
+            }
+        }
+    }
+}
