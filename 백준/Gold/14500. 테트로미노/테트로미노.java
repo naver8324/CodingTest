7bcd4fcf1a2a17e5e35bd287e2afc25a2371c15b
Dusk <59524589+naver8324@users.noreply.github.com>
2025-04-26 06:34:27 +0900

    [Gold IV] Title: 테트로미노, Time: 464 ms, Memory: 31068 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.java
+++ 백준/Gold/14500. 테트로미노/테트로미노.java
@@ -1,76 +1,77 @@
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
 public class Main {
     static int N, M;
-    static int[][] map;
-    static int max = 0;
-
+    static int[][] paper;
+    static int max = -1;
     static int[][][] tetromino = {
-        {{0,0}, {0,1}, {0,2}, {0,3}},
-        {{0,0}, {1,0}, {2,0}, {3,0}},
-
-        {{0,0}, {0,1}, {1,0}, {1,1}},
-
-        {{0,0}, {1,0}, {2,0}, {2,1}},
-        {{0,1}, {1,1}, {2,1}, {2,0}},
-        {{0,0}, {0,1}, {1,0}, {2,0}},
-        {{0,0}, {0,1}, {1,1}, {2,1}},
-
-        {{0,0}, {1,0}, {1,1}, {1,2}},
-        {{0,2}, {1,0}, {1,1}, {1,2}},
-        {{0,0}, {0,1}, {0,2}, {1,2}},
-        {{0,0}, {0,1}, {0,2}, {1,0}},
-
-        {{0,0}, {0,1}, {1,1}, {1,2}},
-        {{0,1}, {1,0}, {1,1}, {2,0}},
-        {{0,0}, {1,0}, {1,1}, {2,1}},
-        {{0,1}, {0,2}, {1,0}, {1,1}},
-
-        {{0,0}, {0,1}, {0,2}, {1,1}},
-        {{0,1}, {1,0}, {1,1}, {2,1}},
-        {{0,1}, {1,0}, {1,1}, {1,2}},
-        {{0,0}, {1,0}, {1,1}, {2,0}},
+            // ㅡ
+            {{0, 0}, {0, 1}, {0, 2}, {0, 3}},
+            {{0, 0}, {1, 0}, {2, 0}, {3, 0}},
+            // ㅁ
+            {{0, 0}, {1, 0}, {0, 1}, {1, 1}},
+            // ㅏ
+            {{0, 0}, {1, 0}, {1, 1}, {2, 0}},
+            {{0, 0}, {0, 1}, {0, 2}, {1, 1}},
+            {{1, 0}, {0, 1}, {1, 1}, {2, 1}},
+            {{0, 1}, {1, 0}, {1, 1}, {1, 2}},
+            //
+            {{0, 1}, {1, 0}, {1, 1}, {2, 0}},
+            {{0, 0}, {0, 1}, {1, 1}, {1, 2}},
+            //
+            {{0, 0}, {1, 0}, {1, 1}, {2, 1}},
+            {{0, 1}, {0, 2}, {1, 0}, {1, 1}},
+            // L
+            {{0, 0}, {1, 0}, {2, 0}, {2, 1}},
+            {{0, 0}, {1, 0}, {0, 1}, {0, 2}},
+            {{0, 0}, {0, 1}, {1, 1}, {2, 1}},
+            {{1, 0}, {1, 1}, {1, 2}, {0, 2}},
+            // J
+            {{0, 1}, {1, 1}, {2, 1}, {2, 0}},
+            {{0, 0}, {1, 0}, {1, 1}, {1, 2}},
+            {{0, 0}, {0, 1}, {1, 0}, {2, 0}},
+            {{0, 0}, {0, 1}, {0, 2}, {1, 2}}
     };
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        M = sc.nextInt();
-        map = new int[N][M];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-        for (int i = 0; i < N; i++)
-            for (int j = 0; j < M; j++)
-                map[i][j] = sc.nextInt();
+        paper = new int[N][M];
 
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                check(i, j);
+        for (int x = 0; x < N; x++) {
+            st = new StringTokenizer(br.readLine());
+            for (int y = 0; y < M; y++) {
+                paper[x][y] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int x = 0; x < N; x++) {
+            for (int y = 0; y < M; y++) {
+                check(x, y);
             }
         }
 
         System.out.println(max);
     }
 
-    static void check(int x, int y) {
+    private static void check(int x, int y) {
         for (int[][] shape : tetromino) {
             int sum = 0;
-            boolean valid = true;
 
             for (int[] block : shape) {
                 int nx = x + block[0];
                 int ny = y + block[1];
 
-                if (nx < 0 || ny < 0 || nx >= N || ny >= M) {
-                    valid = false;
-                    break;
+                if (nx >= 0 && ny >= 0 && nx < N && ny < M) {
+                    sum += paper[nx][ny];
                 }
-
-                sum += map[nx][ny];
             }
 
-            if (valid) {
-                max = Math.max(max, sum);
-            }
+            max = Math.max(max, sum);
         }
     }
 }
