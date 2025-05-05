4e8b3358b41a68c9cc9772d6f09de61c4ee82ebc
Dusk <59524589+naver8324@users.noreply.github.com>
2025-05-05 17:21:48 +0900

    [Silver I] Title: 곱셈, Time: 100 ms, Memory: 14144 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.java
+++ 백준/Silver/1629. 곱셈/곱셈.java
@@ -1,19 +1,30 @@
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static long power(long a, long b, long c) {
-        if (b == 0) return 1;
-        long half = power(a, b / 2, c);
-        long result = (half * half) % c;
-        if (b % 2 == 1) result = (result * a) % c;
-        return result;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        long A = Integer.parseInt(st.nextToken());
+        long B = Integer.parseInt(st.nextToken());
+        long C = Integer.parseInt(st.nextToken());
+
+        System.out.println(power(A, B, C));
     }
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        long A = sc.nextLong();
-        long B = sc.nextLong();
-        long C = sc.nextLong();
-        System.out.println(power(A, B, C));
+    private static long power(long a, long b, long c) {
+        if (b == 0) {
+            return 1;
+        }
+
+        long half = power(a, b / 2, c);
+        long result = (half * half) % c;
+
+        if (b % 2 == 1) {
+            result = (result * a) % c;
+        }
+
+        return result;
     }
 }
