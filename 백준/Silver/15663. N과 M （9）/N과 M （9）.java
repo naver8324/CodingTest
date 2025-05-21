530650216e0d224ad9bc3a21043be476a4763c25
Dusk <59524589+naver8324@users.noreply.github.com>
2025-05-22 08:13:15 +0900

    [Silver II] Title: N과 M (9), Time: 156 ms, Memory: 19028 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.java
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -1,52 +1,52 @@
+import java.io.*;
 import java.util.*;
 
 public class Main {
     static int N, M;
-    static int[] nums;
+    static int[] arr;
+    static int[] selected;
     static boolean[] visited;
-    static int[] result;
     static StringBuilder sb = new StringBuilder();
-    static Set<String> uniqueSequences = new LinkedHashSet<>();
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-        N = sc.nextInt();
-        M = sc.nextInt();
-
-        nums = new int[N];
+        selected = new int[M];
+        arr = new int[N];
         visited = new boolean[N];
-        result = new int[M];
 
+        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            nums[i] = sc.nextInt();
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        Arrays.sort(nums);
+        Arrays.sort(arr);
+
         backtrack(0);
 
-        for (String s : uniqueSequences) {
-            sb.append(s).append("\n");
-        }
-
-        System.out.print(sb);
+        System.out.print(sb.toString());
     }
 
-    static void backtrack(int depth) {
+    private static void backtrack(int depth) {
         if (depth == M) {
-            StringBuilder seq = new StringBuilder();
-            for (int i = 0; i < M; i++) {
-                seq.append(result[i]).append(" ");
+            for (int n : selected) {
+                sb.append(n).append(" ");
             }
-            uniqueSequences.add(seq.toString().trim());
+            sb.append("\n");
             return;
         }
 
+        int prev = -1;
+
         for (int i = 0; i < N; i++) {
-            if (!visited[i]) {
+            if (!visited[i] && prev != arr[i]) {
+                prev = arr[i];
                 visited[i] = true;
-                result[depth] = nums[i];
-                backtrack(depth + 1);
+                selected[depth] = arr[i];
+                backtrack(depth+1);
                 visited[i] = false;
             }
         }
