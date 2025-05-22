c40277720560a013740782db342bc9749979ca7b
Dusk <59524589+naver8324@users.noreply.github.com>
2025-05-23 06:12:20 +0900

    [Silver I] Title: 포도주 시식, Time: 124 ms, Memory: 15044 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.java
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.java
@@ -1,21 +1,22 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
 
-        int n = sc.nextInt();
-        int[] wine = new int[n];
-        for (int i = 0; i < n; i++) {
-            wine[i] = sc.nextInt();
+        int[] wine = new int[N];
+        int[] dp = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            wine[i] = Integer.parseInt(br.readLine());
         }
 
-        int[] dp = new int[n];
-
-        if (n == 1) {
+        if (N == 1) {
             System.out.println(wine[0]);
             return;
-        } else if (n == 2) {
+        } else if (N == 2) {
             System.out.println(wine[0] + wine[1]);
             return;
         }
@@ -24,12 +25,10 @@ public class Main {
         dp[1] = wine[0] + wine[1];
         dp[2] = Math.max(Math.max(wine[0] + wine[1], wine[0] + wine[2]), wine[1] + wine[2]);
 
-        for (int i = 3; i < n; i++) {
-            dp[i] = Math.max(dp[i - 1],
-                     Math.max(dp[i - 2] + wine[i],
-                              dp[i - 3] + wine[i - 1] + wine[i]));
+        for (int i = 3; i < N; i++) {
+            dp[i] = Math.max(Math.max(dp[i - 2] + wine[i], dp[i - 3] + wine[i - 1] + wine[i]), dp[i - 1]);
         }
 
-        System.out.println(dp[n - 1]);
+        System.out.println(dp[N - 1]);
     }
 }
