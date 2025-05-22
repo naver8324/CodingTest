017f8b2195e1995f9f82db30c1b032621e305933
Dusk <59524589+naver8324@users.noreply.github.com>
2025-05-22 10:55:26 +0900

    [Silver II] Title: N과 M (12), Time: 112 ms, Memory: 14432 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N, M;
+    static int[] arr;
+    static int[] selected;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        selected = new int[M];
+        arr = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        backtrack(0, 0);
+
+        System.out.print(sb.toString());
+    }
+
+    private static void backtrack(int depth, int start) {
+        if (depth == M) {
+            for (int n : selected) {
+                sb.append(n).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        int prev = -1;
+
+        for (int i = start; i < N; i++) {
+            if (arr[i] != prev) {
+                prev = arr[i];
+                selected[depth] = arr[i];
+                backtrack(depth + 1, i);
+            }
+        }
+    }
+}
