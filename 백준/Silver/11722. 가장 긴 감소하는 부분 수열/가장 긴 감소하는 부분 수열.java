159310dfce6b5356a2a5341ee1a7cbfec1ebd6ae
Dusk <59524589+naver8324@users.noreply.github.com>
2025-05-29 15:36:08 +0900

    [Silver II] Title: 가장 긴 감소하는 부분 수열, Time: 240 ms, Memory: 21184 KB -BaekjoonHub

---
+++ 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int[] A = new int[N];
+        int[] dp = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            A[i] = sc.nextInt();
+            dp[i] = 1;
+        }
+
+        for (int i = 1; i < N; i++) {
+            for (int j = 0; j < i; j++) {
+                if (A[j] > A[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+
+        int maxLen = 0;
+        for (int i = 0; i < N; i++) {
+            maxLen = Math.max(maxLen, dp[i]);
+        }
+
+        System.out.println(maxLen);
+    }
+}
