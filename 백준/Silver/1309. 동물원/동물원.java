93cc08094d818db58810058387466793de2ba6ae
Dusk <59524589+naver8324@users.noreply.github.com>
2025-06-03 22:49:08 +0900

    [Silver I] Title: 동물원, Time: 100 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Silver/1309. 동물원/동물원.java
+++ 백준/Silver/1309. 동물원/동물원.java
@@ -1,24 +1,27 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int MOD = 9901;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        final int MOD = 9901;
 
-        int[][] dp = new int[N + 1][3];
+        int a = 1;
+        int b = 1;
+        int c = 1;
 
-        dp[1][0] = 1;
-        dp[1][1] = 1;
-        dp[1][2] = 1;
+        for (int i = 1; i < N; i++) {
+            int A = (a + b + c) % MOD;
+            int B = (a + c) % MOD;
+            int C = (a + b) % MOD;
 
-        for (int i = 2; i <= N; i++) {
-            dp[i][0] = (dp[i - 1][0] + dp[i - 1][1] + dp[i - 1][2]) % MOD;
-            dp[i][1] = (dp[i - 1][0] + dp[i - 1][2]) % MOD;
-            dp[i][2] = (dp[i - 1][0] + dp[i - 1][1]) % MOD;
+            a = A;
+            b = B;
+            c = C;
         }
 
-        int result = (dp[N][0] + dp[N][1] + dp[N][2]) % MOD;
-        System.out.println(result);
+        System.out.println((a + b + c) % MOD);
     }
 }
