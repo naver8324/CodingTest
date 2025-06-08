6501b2868901e63eefcfa43b3efab2b791d3d261
Dusk <59524589+naver8324@users.noreply.github.com>
2025-06-08 23:41:57 +0900

    [Silver I] Title: 카드 구매하기, Time: 132 ms, Memory: 14540 KB -BaekjoonHub

--- 백준/Silver/11052. 카드 구매하기/카드 구매하기.java
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.java
@@ -1,13 +1,16 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
 
-        int N = sc.nextInt();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
         int[] P = new int[N + 1];
         for (int i = 1; i <= N; i++) {
-            P[i] = sc.nextInt();
+            P[i] = Integer.parseInt(st.nextToken());
         }
 
         int[] dp = new int[N + 1];
