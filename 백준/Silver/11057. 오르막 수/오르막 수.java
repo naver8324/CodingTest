4e46bd00a308a0b3e0fc6da0f44ccba0480d7c33
Dusk <59524589+naver8324@users.noreply.github.com>
2025-06-10 02:32:47 +0900

    [Silver I] Title: 오르막 수, Time: 104 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Silver/11057. 오르막 수/오르막 수.java
+++ 백준/Silver/11057. 오르막 수/오르막 수.java
@@ -1,27 +1,29 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
         final int MOD = 10007;
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
 
-        int[][] dp = new int[N + 1][10];
+        int[][] dp = new int[N][10];
 
         for (int i = 0; i < 10; i++) {
-            dp[1][i] = 1;
+            dp[0][i] = 1;
         }
 
-        for (int n = 2; n <= N; n++) {
-            dp[n][0] = dp[n - 1][0];
-            for (int d = 1; d < 10; d++) {
-                dp[n][d] = (dp[n][d - 1] + dp[n - 1][d]) % MOD;
+        for (int i = 1; i < N; i++) {
+            dp[i][0] = dp[i - 1][0];
+
+            for (int j = 1; j < 10; j++) {
+                dp[i][j] = (dp[i - 1][j] + dp[i][j - 1]) % MOD;
             }
         }
 
         int result = 0;
         for (int i = 0; i < 10; i++) {
-            result = (result + dp[N][i]) % MOD;
+            result = (result + dp[N - 1][i]) % MOD;
         }
 
         System.out.println(result);
