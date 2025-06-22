f4864dfa3fd471db61a768f486ee640a0fcedce9
Dusk <59524589+naver8324@users.noreply.github.com>
2025-06-22 21:24:28 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 720 ms, Memory: 119128 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -8,25 +8,13 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        int[][] arr = new int[N + 1][N + 1];
         int[][] prefixSum = new int[N + 1][N + 1];
 
         for (int i = 1; i <= N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 1; j <= N; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
+                prefixSum[i][j] = Integer.parseInt(st.nextToken()) + prefixSum[i - 1][j] + prefixSum[i][j - 1] - prefixSum[i - 1][j - 1];
 
-        prefixSum[1][1] = arr[1][1];
-        for (int i = 2; i <= N; i++) {
-                prefixSum[i][1] = prefixSum[i-1][1] + arr[i][1];
-                prefixSum[1][i] = prefixSum[1][i-1] + arr[1][i];
-        }
-
-        for (int i = 2; i <= N; i++) {
-            for (int j = 2; j <= N; j++) {
-                prefixSum[i][j] = arr[i][j] + prefixSum[i - 1][j] + prefixSum[i][j - 1] - prefixSum[i - 1][j - 1];
             }
         }
 
