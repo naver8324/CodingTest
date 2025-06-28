174a3b864fcf3a206697523bddfdab7c6fb2df2c
Dusk <59524589+naver8324@users.noreply.github.com>
2025-06-28 17:52:46 +0900

    [Silver IV] Title: ATM, Time: 120 ms, Memory: 14356 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.java
+++ 백준/Silver/11399. ATM/ATM.java
@@ -1,51 +1,32 @@
 import java.io.*;
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
         int[] arr = new int[N];
 
         for (int i = 0; i < N; i++) {
-            arr[i] = sc.nextInt();
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
         for (int i = 1; i < N; i++) {
-            int insert_point = i;
-            int insert_value = arr[i];
-
-            for (int j = i - 1; j >= 0; j--) {
-                if (arr[j] < arr[i]) {
-                    insert_point = j + 1;
-                    break;
-                }
-                if (j == 0) {
-                    insert_point = 0;
+            for (int j = 0; j < i; j++) {
+                if (arr[i] < arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
                 }
             }
-
-            for (int j = i; j > insert_point; j--) {
-                arr[j] = arr[j - 1];
-            }
-
-            arr[insert_point] = insert_value;
-        }
-
-        int[] prefixSum = new int[N];
-        prefixSum[0] = arr[0];
-
-        for(int i = 1; i < N; i++) {
-            prefixSum[i] = prefixSum[i - 1] + arr[i];
         }
 
         int sum = 0;
-        for(int num : prefixSum) {
-            sum += num;
+        for (int i=0; i<N; i++) {
+            sum += arr[i] * (N - i);
         }
 
         System.out.println(sum);
-        sc.close();
     }
 }
