d80a776fe1285f75d6de8ad6d8ee12429088d9c3
Dusk <59524589+naver8324@users.noreply.github.com>
2025-06-30 19:18:18 +0900

    [Bronze V] Title: We love kriii, Time: 100 ms, Memory: 13876 KB -BaekjoonHub

---
+++ 백준/Bronze/10718. We love kriii/We love kriii.java
@@ -0,0 +1,8 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println("강한친구 대한육군\n강한친구 대한육군");
+    }
+}
