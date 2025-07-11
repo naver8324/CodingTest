86d24e53b871a0bfb0239ef6a69a7e129e1aef55
Dusk <59524589+naver8324@users.noreply.github.com>
2025-07-11 22:14:28 +0900

    [Bronze IV] Title: 영수증, Time: 100 ms, Memory: 14152 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int X = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
+
+        int sum = 0;
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+
+            sum += A * B;
+        }
+
+        System.out.println(X == sum ? "Yes" : "No");
+    }
+}
