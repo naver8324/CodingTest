38109614a145b0cd69c5d42fdc1771895e694e2d
Dusk <59524589+naver8324@users.noreply.github.com>
2025-07-22 22:15:14 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 104 ms, Memory: 14140 KB -BaekjoonHub

---
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException{
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<N; i++){
+            for(int j=i; j>0; j--){
+                sb.append(" ");
+            }
+            for(int j=N-i; j>0; j--){
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+
+        System.out.print(sb.toString());
+    }
+}
