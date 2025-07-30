131dbdf7543cb5ed43d864dd46024c1bc158fb4b
Dusk <59524589+naver8324@users.noreply.github.com>
2025-07-31 02:52:10 +0900

    [Bronze II] Title: 공 바꾸기, Time: 104 ms, Memory: 14264 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[] basket = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            basket[i] = i + 1;
+        }
+
+        for (int n = 0; n < M; n++) {
+            st = new StringTokenizer(br.readLine());
+            int i = Integer.parseInt(st.nextToken())-1;
+            int j = Integer.parseInt(st.nextToken())-1;
+
+            int temp = basket[i];
+            basket[i] = basket[j];
+            basket[j] = temp;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int n : basket) {
+            sb.append(n).append(" ");
+        }
+
+        System.out.print(sb.toString());
+    }
+}
