3a87e3af1d44374c32dd9f8261494b9308708b68
Dusk <59524589+naver8324@users.noreply.github.com>
2025-08-11 05:43:33 +0900

    [Bronze IV] Title: 평균 점수, Time: 96 ms, Memory: 14124 KB -BaekjoonHub

---
+++ 백준/Bronze/10039. 평균 점수/평균 점수.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int sum = 0;
+
+        for (int i = 0; i < 5; i++) {
+            int n = Integer.parseInt(br.readLine());
+            if (n < 40) {
+                n = 40;
+            }
+
+            sum += n;
+        }
+
+        System.out.println(sum / 5);
+    }
+}
