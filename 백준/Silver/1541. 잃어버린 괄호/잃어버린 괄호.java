e69a71d6f2e15e7e1891d0a6703972036c05f817
Dusk <59524589+naver8324@users.noreply.github.com>
2025-08-13 08:00:23 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 100 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -4,24 +4,24 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] N = br.readLine().split("-");
+        String input = br.readLine();
+        String[] splitMinus = input.split("-");
+        int answer = 0;
 
-        int result = 0;
+        String[] firstPlus = splitMinus[0].split("\\+");
 
-        String[] expression = N[0].split("\\+");
-
-        for (String s : expression) {
-            result += Integer.parseInt(s);
+        for (String n : firstPlus) {
+            answer += Integer.parseInt(n);
         }
 
-        for (int i = 1; i < N.length; i++) {
-            expression = N[i].split("\\+");
+        for (int i = 1; i < splitMinus.length; i++) {
+            String[] splitPlus = splitMinus[i].split("\\+");
 
-            for (String s : expression) {
-                result -= Integer.parseInt(s);
+            for (String plus : splitPlus) {
+                answer -= Integer.parseInt(plus);
             }
         }
 
-        System.out.println(result);
+        System.out.println(answer);
     }
 }
