a2827c78eac5ad0f2e393e55007e9dcfe060162e
Dusk <59524589+naver8324@users.noreply.github.com>
2025-08-12 23:51:07 +0900

    [Gold V] Title: 회의실 배정, Time: 472 ms, Memory: 43296 KB -BaekjoonHub

--- 백준/Gold/1931. 회의실 배정/회의실 배정.java
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.java
@@ -1,49 +1,39 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
 
-        List<Meeting> meetings = new ArrayList<>();
-        int maxCount = 0;
+        int[][] A = new int[N][2];
 
         for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-
-            meetings.add(new Meeting(start, end));
+            A[i][0] = Integer.parseInt(st.nextToken());
+            A[i][1] = Integer.parseInt(st.nextToken());
         }
 
-        meetings.sort((a, b) -> {
-            if (a.end != b.end) {
-                return a.end - b.end;
-            } else {
-                return a.start - b.start;
+        int count = 0;
+        int end = -1;
+
+        Arrays.sort(A, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if (o1[1] == o2[1]) {
+                    return o1[0] - o2[0];
+                }
+                return o1[1] - o2[1];
             }
         });
 
-        int now = 0;
-
-        for (Meeting m : meetings) {
-            if (now <= m.start) {
-                maxCount++;
-                now = m.end;
+        for (int i = 0; i < N; i++) {
+            if (end <= A[i][0]) {
+                end = A[i][1];
+                count++;
             }
         }
 
-        System.out.println(maxCount);
-    }
-}
-
-class Meeting {
-    int start;
-    int end;
-
-    public Meeting(int start, int end) {
-        this.start = start;
-        this.end = end;
+        System.out.println(count);
     }
 }
