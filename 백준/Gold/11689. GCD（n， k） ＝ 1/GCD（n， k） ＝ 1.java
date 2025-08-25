255ace7b16feb0ad00ac370afd19b77a45295425
Dusk <59524589+naver8324@users.noreply.github.com>
2025-08-25 20:33:13 +0900

    [Gold I] Title: GCD(n, k) = 1, Time: 116 ms, Memory: 14280 KB -BaekjoonHub

---
+++ 백준/Gold/11689. GCD（n， k） ＝ 1/GCD（n， k） ＝ 1.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long n = Long.parseLong(br.readLine());
+        long result = n;
+
+        for (long i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
+                result = result - result / i;
+
+                while (n % i == 0) {
+                    n /= i;
+                }
+            }
+        }
+
+        if (n > 1) {
+            result = result - result / n;
+        }
+        System.out.println(result);
+    }
+}
