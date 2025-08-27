7fc9ad1855c8e5ed6a0b0e9daebcb18c33f0c188
Dusk <59524589+naver8324@users.noreply.github.com>
2025-08-27 22:27:18 +0900

    [Silver I] Title: 최대공약수, Time: 228 ms, Memory: 29956 KB -BaekjoonHub

---
+++ 백준/Silver/1850. 최대공약수/최대공약수.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long A = Long.parseLong(st.nextToken());
+        long B = Long.parseLong(st.nextToken());
+
+        long oneCount = gcd(A, B);
+        sb.append("1".repeat((int) oneCount));
+
+        System.out.print(sb.toString());
+    }
+
+    private static long gcd(long a, long b) {
+        while (b != 0) {
+            long temp = a % b;
+            a = b;
+            b = temp;
+        }
+
+        return a;
+    }
+}
