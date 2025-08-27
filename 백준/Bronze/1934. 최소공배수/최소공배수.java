e47af715f5c1eb7bb3da7c6e98621e1f7f19eb89
Dusk <59524589+naver8324@users.noreply.github.com>
2025-08-27 22:12:54 +0900

    [Bronze I] Title: 최소공배수, Time: 120 ms, Memory: 14508 KB -BaekjoonHub

---
+++ 백준/Bronze/1934. 최소공배수/최소공배수.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+
+        while (T-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+
+            int lcm = A * B / gcd(A, B);
+            sb.append(lcm).append("\n");
+        }
+
+        System.out.print(sb.toString());
+    }
+
+    private static int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = a % b;
+            a = b;
+            b = temp;
+        }
+
+        return a;
+    }
+}
