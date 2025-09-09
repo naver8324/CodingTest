ba6113203808060c5e984587881678405b087474
Dusk <59524589+naver8324@users.noreply.github.com>
2025-09-10 05:55:38 +0900

    [Bronze III] Title: 약수 구하기, Time: 176 ms, Memory: 17704 KB -BaekjoonHub

---
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+        
+        List<Integer> divisors = new ArrayList<>();
+        
+        for (int i = 1; i <= N; i++) {
+            if (N % i == 0) {
+                divisors.add(i);
+            }
+        }
+        
+        if (divisors.size() < K) {
+            System.out.println(0);
+        } else {
+            System.out.println(divisors.get(K - 1));
+        }
+    }
+}
