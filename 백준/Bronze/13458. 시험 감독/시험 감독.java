816c8374d414fd1f42cf7b76388ef5a336698f3d
Dusk <59524589+naver8324@users.noreply.github.com>
2025-09-23 22:58:13 +0900

    [Bronze II] Title: 시험 감독, Time: 420 ms, Memory: 92220 KB -BaekjoonHub

--- 백준/Bronze/13458. 시험 감독/시험 감독.java
+++ 백준/Bronze/13458. 시험 감독/시험 감독.java
@@ -4,31 +4,33 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
         int N = Integer.parseInt(br.readLine());
+        int[] place = new int[N];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        
-        int[] A = new int[N];
+
         for (int i = 0; i < N; i++) {
-            A[i] = Integer.parseInt(st.nextToken());
+            place[i] = Integer.parseInt(st.nextToken());
         }
-        
+
         st = new StringTokenizer(br.readLine());
         int B = Integer.parseInt(st.nextToken());
-        int C = Integer.parseInt(st.nextToken()); 
-        
-        long result = 0;
-        
-        for (int i = 0; i < N; i++) {
-            result++;
-            int remain = A[i] - B;
-            
-            if (remain > 0) {
-                result += remain / C;
-                if (remain % C != 0) result++;
+        int C = Integer.parseInt(st.nextToken());
+
+        long count = 0;
+
+        for (int p : place) {
+            p -= B;
+            count++;
+
+            if (p > 0) {
+                count += p / C;
+
+                if (p % C != 0) {
+                    count++;
+                }
             }
         }
-        
-        System.out.println(result);
+
+        System.out.println(count);
     }
 }
