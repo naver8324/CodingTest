046138ff48947371a41bbf9fec11f0a8672e0f63
Dusk <59524589+naver8324@users.noreply.github.com>
2025-10-15 20:40:37 +0900

    [Bronze I] Title: 단어 뒤집기, Time: 404 ms, Memory: 41464 KB -BaekjoonHub

--- 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.java
+++ 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.java
@@ -1,20 +1,17 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
-
         StringBuilder sb = new StringBuilder();
 
         for (int i = 0; i < T; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
+
             while (st.hasMoreTokens()) {
-                String word = st.nextToken();
-                sb.append(new StringBuilder(word).reverse()).append(" ");
+                sb.append(new StringBuilder(st.nextToken()).reverse()).append(" ");
             }
             sb.append("\n");
         }
